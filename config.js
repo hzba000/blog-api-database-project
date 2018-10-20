6b7a402c6b80d6c24d380520e250f6417bde91f8
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-10-19 19:09:41 -0700

    Updated Remote Database on mLab

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/tempTestDb";
+  process.env.DATABASE_URL || "mongodb://thinkful:thinkful1@ds043350.mlab.com:43350/blog-api-database";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-tempTestDb";
 exports.PORT = process.env.PORT || 8090;
+
